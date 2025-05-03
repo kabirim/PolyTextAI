dfc91e249827759d927c524ea38e1a64640c429a
kabirim <mo.kabiiri@gmail.com>
2025-05-03 21:51:05 +0200

    added summarized

--- src/predict/predict.py
+++ src/predict/predict.py
@@ -5,5 +5,4 @@ def predict(input_dict):
     with open('src/models/models/model.pkl', 'rb') as f:
         model = pickle.load(f)
     input_df = pd.DataFrame([input_dict])
-    input_df
     return model.predict(input_df)[0]
