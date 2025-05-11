bc90c916eb69511ecddde9e81c5e53fa2b2252fa
kabirim <mo.kabiiri@gmail.com>
2025-05-11 22:22:34 +0200

    modified gitignore

---
+++ src/models/automatic_text_completion_model.py
@@ -0,0 +1,25 @@
+# importing the BART model and tokenizer 
+from transformers import BartForConditionalGeneration, BartTokenizer
+
+# loading the pretrained weights for BART
+# here, we use facebook's bart-large model
+bart_model = BartForConditionalGeneration.from_pretrained("facebook/bart-large",
+                                                     forced_bos_token_id=0) # takes a while to load
+# loading the raw text tokenizer for the BART model
+tokenizer = BartTokenizer.from_pretrained("facebook/bart-large")
+
+def generte_automatic_text_completion(sentence):
+ 
+    # preprocessing(tokenizing) the text as input for the BART model 
+    tokenized_sent = tokenizer(sentence, return_tensors="pt")
+
+    # generated encoded ids
+    generated_encoded = bart_model.generate(tokenized_sent['input_ids'])
+
+    # decoding the generated encoded ids
+    prediction = tokenizer.batch_decode(generated_encoded, skip_special_tokens=True)[0]
+    return prediction
+
+if __name__ == '__main__':
+    sent = "GeekforGeeks has a <mask> article on BART."
+    generte_automatic_text_completion(sent)
