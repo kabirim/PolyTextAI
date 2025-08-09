ce050f45db26596e4798c747c67eb30287b05a26
kabirim <mo.kabiiri@gmail.com>
2025-08-09 09:38:56 +0200

    refacto and fixedBug

--- src/api/main.py
+++ src/api/main.py
@@ -38,37 +38,37 @@ app = FastAPI(
 #     prediction = predict(input_dict)
 #     return {"prediction": int(prediction)}
 
-@app.post("/text/clean",status_code=status.HTTP_200_OK )
+@app.post("/api/text/clean",status_code=status.HTTP_200_OK )
 async def clean_text(input: InputText):
-    result = asyncio.run(cleaner_raw_text(input.text))
+    result = cleaner_raw_text(input.text)
     return {"Output": result}
 
-@app.post("/text/summary",status_code=status.HTTP_200_OK )
+@app.post("/api/text/summary",status_code=status.HTTP_200_OK )
 async def summarize_text(input: InputText):
     result = asyncio.run(summairizing(input.text))
     return {"Output": result}
 
-@app.post("/predict/next-word",status_code=status.HTTP_200_OK)
+@app.post("/api/predict/next-word",status_code=status.HTTP_200_OK)
 async def predict_next_word(input: InputText):
     result = predictNextWord(input.text)
     return {"Output": result}
 
-@app.post("/qa/predict",status_code=status.HTTP_200_OK)
+@app.post("/api/qa/predict",status_code=status.HTTP_200_OK)
 async def predict_answer(input: InputQA):
     result = predictAnswer(input)
     return {"Output": result}
 
-@app.post("/text/autocorrect",status_code=status.HTTP_200_OK)
+@app.post("/api/text/autocorrect",status_code=status.HTTP_200_OK)
 async def autocorrect_text(input: InputText):
     result = get_best_correction(input.text,3)
     return {"Output": result}
 
-@app.post("/text/completion",status_code=status.HTTP_200_OK)
+@app.post("/api/text/completion",status_code=status.HTTP_200_OK)
 async def complete_text(input: InputText):
     result = predict_automatic_text_completion(input.text)
     return {"Output": result}
 
-@app.post("/text/ner",status_code=status.HTTP_200_OK)
+@app.post("/api/text/ner",status_code=status.HTTP_200_OK)
 async def named_entity_recognition(input: InputText):
     result = predict_named_entity_recongnition(input.text)
     return {"Output": result}
